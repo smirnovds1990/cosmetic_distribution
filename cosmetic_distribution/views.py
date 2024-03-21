4e11e8a3953b1900eca0d72e751e409a1981e8f6
smirnovds1990 <you@example.comsmirnovds1990@gmail.com>
2024-03-21 11:13:53 +0300

    refactor(templates): create base, footer and fix sign_up and login. Also add error cathcing to their views.

--- cosmetic_distribution/views.py
+++ cosmetic_distribution/views.py
@@ -1,5 +1,8 @@
-from flask import redirect, render_template, request, url_for
-from flask_login import login_user
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import login_user, logout_user
+# from sqlalchemy.exc import IntegrityError
+from werkzeug.security import check_password_hash
+# from werkzeug.security import generate_password_hash
 
 from . import app, db, login_manager
 from .models import User
@@ -10,44 +13,53 @@ def loader_user(user_id):
     return User.query.get(user_id)
 
 
-@app.route('/register', methods=["GET", "POST"])
-def register():
-  # If the user made a POST request, create a new user
-    if request.method == "POST":
-        user = User(
-            username=request.form.get("username"),
-            password=request.form.get("password")
-        )
-        # Add the user to the database
-        db.session.add(user)
-        # Commit the changes made
-        db.session.commit()
-        # Once user account created, redirect them
-        # to login route (created later on)
-        return redirect(url_for("login"))
-    # Renders sign_up template if user made a GET request
-    return render_template("sign_up.html")
+# Only for administration! To create a new user. Don't delete!
+# @app.route('/register', methods=["GET", "POST"])
+# def register():
+#     if request.method == "POST":
+#         password = request.form.get("password")
+#         if not password or len(password) < 8:
+#             flash('Введите пароль не менее 8 символов.')
+#         else:
+#             hashed_password = generate_password_hash(password)
+#             try:
+#                 user = User(
+#                     username=request.form.get("username"),
+#                     password=hashed_password
+#                 )
+#                 db.session.add(user)
+#                 db.session.commit()
+#                 return redirect(url_for("login"))
+#             except IntegrityError:
+#                 db.session.rollback()
+#                 flash('Пользователь с таким именем уже существует.')
+#     return render_template("sign_up.html")
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/", methods=["GET", "POST"])
 def login():
-    # If a post request was made, find the user by
-    # filtering for the username
     if request.method == "POST":
         user = User.query.filter_by(
             username=request.form.get("username")).first()
-        # Check if the password entered is the
-        # same as the user's password
-        if user.password == request.form.get("password"):
-            # Use the login_user method to log in the user
+        if not user:
+            flash('Такого пользователя нет', 'error')
+            return render_template("login.html")
+        hashed_password = user.password
+        form_password = request.form.get("password")
+        if check_password_hash(hashed_password, form_password):
             login_user(user)
             return redirect(url_for("home"))
-        # Redirect the user back to the home
-        # (we'll create the home route in a moment)
+        else:
+            flash('Неправильный пароль', 'error')
     return render_template("login.html")
-
-
-@app.route("/")
-def home():
-    # Render home.html on "/" route
-    return render_template("home.html")
