7f574814b6286455f38b06472ad8ce8b48a4fe32
smirnovds1990 <you@example.comsmirnovds1990@gmail.com>
2024-03-22 13:07:55 +0300

    fix (templates): add csrf_token to login.html, sign_up.html.

--- cosmetic_distribution/__init__.py
+++ cosmetic_distribution/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 from settings import Config
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
